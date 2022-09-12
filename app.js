d4f9bc73d8927e6bbb915643ae1c71a8ec92cd41
nahueediaz <nahuelnicolas.d.99@gmail.com>
2022-09-12 19:22:11 -0300

    cree eccomerce de hamburgueseria

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ alert(bisiesto + " años bisiestos desde 2022")
 }*/
 
 // Conversor de monedas de pesos a dolar o euro
-
+/*
 let numero = parseInt(prompt(`¿Que operacion desea realizar?
 1- Cambio de peso a dolar
 2- Cambio de peso a euro`))
@@ -39,4 +39,107 @@ else if (numero == 2){
 
 else {
     alert("Caracter ingresado incorrecto")
+}*/
+
+//ecommcerce de compra de hamburguesas
+function Hamburguesa(id, nombre, precio, tipo, stock) {
+    this.id = id
+    this.nombre = nombre
+    this.precio = precio
+    this.detalle = tipo
+    this.stock = stock
+
+    this.restarStock = (cantidad) => {
+    this.stock -= cantidad
+    }
 }
+
+const hamburguesas = [
+    new Hamburguesa(1, "Veggie", 800, "comida", 12),
+    new Hamburguesa(2, "Italiana", 850, "comida", 10),
+    new Hamburguesa(3, "Queso grillado", 870, "comida", 15),
+    new Hamburguesa(4, "Crazy chicken", 820, "comida", 14),
+    new Hamburguesa(5, "Big kiro", 900, "comida", 15),
+    new Hamburguesa(6, "Classic", 750, "comida", 15),
+    new Hamburguesa(7, "Kiro bacon", 770, "comida", 10),
+    new Hamburguesa(8, "Agua", 200, "bebida", 20),
+    new Hamburguesa(9, "Gaseosa", 250, "bebida", 12),
+    new Hamburguesa(10, "Cerveza", 800, "bebida", 12),
+]
+//menu de hamburguesas
+
+function Menu() {
+    this.hamburguesas = []
+    
+    this.agregarHamburguesa = (hamburguesa) => {
+    this.hamburguesas = [...this.hamburguesas, hamburguesa]
+    }
+
+    this.traerHamburguesa = (id) => {
+        return this.hamburguesas.find(hamburguesa => hamburguesa.id == id)
+    }
+
+    this.mostrarMenu = () => {
+        return this.hamburguesas.map(hamburguesa => `${hamburguesa.id}. ${hamburguesa.nombre}`).join("\n")
+    }
+}
+
+const menu = new Menu()
+
+for(const hamburguesa of hamburguesas){
+    menu.agregarHamburguesa(hamburguesa)
+}
+//carrito para compra de hamburguesas
+
+function Carrito(){
+    this.total = 0
+    this.hamburguesas = []
+
+    this.mostrarCompra = () => {
+    return this.hamburguesas
+        .map((hamburguesa) => `${hamburguesa.nombre}: ${hamburguesa.precio}`)
+        .join("\n");
+    };
+
+    this.agregarAlCarrito = (id, cantidad) => {
+    const hamburguesa = menu.traerHamburguesa(id)
+
+// Si se piden mas hamburguesas de las que hay en stock te pide que ingreses un numero menor
+    if(hamburguesa.stock < cantidad) {
+        alert(
+            `No se puede pedir mas hamburguesas de las que hay en stock(${hamburguesa.stock}).`
+        );
+        return false
+    }
+
+        hamburguesa.restarStock(cantidad)
+
+        this.hamburguesas = [...this.hamburguesas, hamburguesa]
+      this.total += hamburguesa.precio * cantidad
+        return true
+    }
+}
+// Aca indicas si seguis comprando
+
+const carrito = new Carrito()
+
+let end = "no"
+//Si es distinto a si continuas con la compra
+
+while(end !== "si"){
+    const id = Number(prompt(`Seleccione el numero de producto a comprar: \n${menu.mostrarMenu()}`))
+    const cantidad = Number(prompt("Indique la cantidad que quiere del producto seleccionado: "))
+    let resultado = carrito.agregarAlCarrito(id, cantidad)
+    
+    while(!resultado){
+        const nuevaCantidad = Number(prompt(
+        "Selecciona una nueva cantidad"
+        ))
+        resultado = carrito.agregarAlCarrito(id, nuevaCantidad)
+    }
+
+//indicar si para terminar con la compra
+    end = prompt("Escribe si cuando haya terminado de elegir sus hamburguesas")?.toLowerCase()
+}
+//Total de los productos seleccionados. Y ademas indica cuanto sale cada una.
+alert(`El total de su compra es: $${carrito.total}. \n Has comprado: ${carrito.mostrarCompra()}\nMuchas gracias por su compra que lo disfrute`)
