e9d7ff00c82027d0599e233fa6026412cbf4651f
nahueediaz <nahuelnicolas.d.99@gmail.com>
2022-09-19 22:45:37 -0300

    añadi DOM

--- app.js
+++ app.js
@@ -42,29 +42,64 @@ else {
 }*/
 
 //ecommcerce de compra de hamburguesas
-function Hamburguesa(id, nombre, precio, tipo, stock) {
+function Hamburguesa(id, nombre, precio, tipo, stock, detalle) {
     this.id = id
     this.nombre = nombre
     this.precio = precio
     this.tipo = tipo
     this.stock = stock
+    this.detalle = detalle
 
     this.restarStock = (cantidad) => {
     this.stock -= cantidad
     }
 }
 
-const hamburguesas = [
-    new Hamburguesa(1, "Veggie", 800, "comida", 12),
-    new Hamburguesa(2, "Italiana", 850, "comida", 10),
-    new Hamburguesa(3, "Queso grillado", 870, "comida", 15),
-    new Hamburguesa(4, "Queso azul", 820, "comida", 14),
-    new Hamburguesa(5, "Big kiro", 900, "comida", 15),
-    new Hamburguesa(6, "Classic", 750, "comida", 15), 
-    new Hamburguesa(7, "Agua", 200, "bebida", 20),
-    new Hamburguesa(8, "Gaseosa", 250, "bebida", 12),
-    new Hamburguesa(9, "Cerveza", 300, "bebida", 12),
+let hamburguesas = [
+    new Hamburguesa(1, "Veggie", 800, "comida", 12, "Pan de espinaca, medallon vegetariano, tomate, queso, cebolla."),
+    new Hamburguesa(2, "Italiana", 850, "comida", 10, "Burger, muzzarella, tomate asado, rucula y alioli verde."),
+    new Hamburguesa(3, "Queso grillado", 870, "comida", 15,"Burger con provoleta a la plancha, morrones asados, cebolla frita, rucula."),
+    new Hamburguesa(4, "Queso azul", 820, "comida", 14, "Burger, cebolla dulce, mayonesa, queso azul, sesamo tostado."),
+    new Hamburguesa(5, "Big kiro", 900, "comida", 15, "Doble burger, cheddar, cebolla caramelizada."),
+    new Hamburguesa(6, "Classic", 750, "comida", 15, "Burger, pepinillos, tomate, lechuga, mayonesa de morrones"), 
+    new Hamburguesa(7, "Agua", 200, "bebida", 20, "Agua"),
+    new Hamburguesa(8, "Gaseosa", 250, "bebida", 12, "Pepsi, fanta, sprite"),
+    new Hamburguesa(9, "Cerveza", 300, "bebida", 12, "Brahama, Heinneken"),
 ]
+
+//DOM 
+
+let contenedor = document.getElementById("contenedor-productos")
+let temp = document.querySelector("template")
+let card = temp.content.querySelector("div")
+
+function crear (carta){
+    contenedor.innerHTML = ""
+
+    carta.forEach(hamburguesa => {
+        let clonarMenu = card.cloneNode(true)
+        contenedor.appendChild(clonarMenu)
+
+        console.log(clonarMenu)
+        //Nombre del producto
+        clonarMenu.children[0].innerText = hamburguesa.nombre
+
+        //precio
+
+        clonarMenu.children[1].innerText = hamburguesa.precio
+
+        // detalle de las burger
+        clonarMenu.children[2].innerText = hamburguesa.detalle
+    });
+}
+
+crear (hamburguesas)
+
+
+
+
+
+/*
 // Metodo filter mostrar solo como funciona, no interactua con el resto del codigo
     const comid = hamburguesas.filter(burger=> burger.tipo == "comida")
     const bebid = hamburguesas.filter(drinks => drinks.tipo == "bebida")
@@ -152,3 +187,4 @@ let nombre = prompt("Ingrese su nombre")
 let email = prompt("Ingrese su email")
 
 alert(`Muchas gracias por su compra ${nombre} por medio de su email ${email} nos pondremos en contacto con usted!`)
+*/
